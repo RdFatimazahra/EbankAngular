6dce762e4d57332693b0f64a0d99f2b04ff19017
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-07-18 11:02:36 +0100

    Creation de service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { CompteComponent } from './compte/compte.component';
 import { CarteComponent } from './carte/carte.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     CompteComponent,
-    CarteComponent
+    CarteComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
