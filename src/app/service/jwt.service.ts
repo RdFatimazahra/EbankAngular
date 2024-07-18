d864a73f34de82a59d027e22c469a938f8d6868d
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-07-18 11:32:23 +0100

    Lier jwtservice avec l api register

--- src/app/service/jwt.service.ts
+++ src/app/service/jwt.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+
+const BASE_URL  = ["http://localhost:8081/api/v1/auth/"]
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
 
-  constructor() { }
+  constructor( private http : HttpClient) { }
+
+  register(signRequest:any): Observable<any>{
+    return this.http.post(BASE_URL+'register', signRequest)
+
+  }
 }
